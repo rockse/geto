7d3cdafb54035f06c7763fcba9f5e44db572d40d
Abhishek Gupta <abhishek2411@outlook.com>
2015-05-26 19:32:59 +0530

    get parameters from URL

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import web
 render = web.template.render('templates/')
 
 urls = (
-  '/', 'index'
+  '/(.*)', 'index'
 )
 
 class index:
-    def GET(self):
-        i = web.input(name=None)
-        return render.index(i.name)
+    def GET(self, name):
+        return render.index(name)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
